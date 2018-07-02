e65c94e0ad13cdb34b97f2098c6a39425842637f
Paul Spata <paul@fortifygroup.com>
2018-07-02 17:58:10 -0400

    debug messages in image controller

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
@@ -51,7 +51,7 @@ namespace ImageResizeWebApp.Controllers
 
                     return BadRequest("Please provide a name for your image container in the azure blob storage");
                 
-                exs+=" Finished Initial Validation; ";
+                exs+=" Finished Initial Validation: Count="+files.Count+"; ";
                 var i = 0;
                 foreach (var formFile in files)
                 {
